c824a39aff2b287e4b4416e54ec121aaa74e6429
Jordan Slater <slatjord@gmail.com>
2021-07-24 00:39:19 -0400

    Gyro working!

--- src/logger/src/accel_tracker_node.cpp
+++ src/logger/src/accel_tracker_node.cpp
@@ -11,42 +11,62 @@
 // geometry_msgs::TransformStamped transformStamped;
 tf2::Stamped<tf2::Transform> transformStamped;
 
+void UpdateTransform(geometry_msgs::Accel accel, double dt)
+{
+    // this was tuned when dt was roughly 0.054 to 0.066
+    const double alpha = 0.94;
+
+    static double gx, gy, gz;
+    static bool first = true;
+    if (first)
+    {
+        gx = accel.angular.x;
+        gy = accel.angular.y;
+        gz = accel.angular.z;
+        first = false;
+        return;
+    } else {
+        gx = gx * (1 - alpha) + accel.angular.x * alpha;
+        gy = gy * (1 - alpha) + accel.angular.y * alpha;
+        gz = gz * (1 - alpha) + accel.angular.z * alpha;
+    }
+
+    // ROS_INFO_STREAM("\n gx: " << gx << "\n gy: " << gy << "\n gz: " << gz);
+
+    transformStamped.setOrigin(tf2::Vector3(0, 0, 0));
+    double roll, pitch, yaw;
+    tf2::Matrix3x3(transformStamped.getRotation()).getRPY(roll, pitch, yaw);
+    tf2::Quaternion new_rotation;
+    const double IDKWHY = 1.0;
+    const double degToRad = M_PI / 180.0;
+    new_rotation.setRPY(
+        roll + degToRad * gx * dt * IDKWHY,
+        pitch + degToRad * gy * dt * IDKWHY,
+        yaw + degToRad * gz * dt * IDKWHY);
+
+    ROS_INFO_STREAM("dt: " << dt);
+    transformStamped.setRotation(new_rotation);
+}
+
 void accelCallback(const geometry_msgs::AccelStamped& accel_msg){
     static tf2_ros::TransformBroadcaster br;
     double dt = (accel_msg.header.stamp - transformStamped.stamp_).toSec();
 
-    transformStamped.stamp_ = ros::Time::now();
-    transformStamped.frame_id_ = "map";
-    // transformStamped.child_frame_id = "mpu2";
-
-    transformStamped.setOrigin(tf2::Vector3(0, 0, 0));
-    // transformStamped.transform.translation.x += accel_msg.accel.linear.x * dt * dt;
-    // transformStamped.transform.translation.y += accel_msg.accel.linear.y * dt * dt;
-    // // subtract one g of gravity
-    // transformStamped.transform.translation.z += (accel_msg.accel.linear.z - 1) * dt * dt;
-
-    // tf2::Vector3 translation;
-    // tf2::fromMsg(transformStamped.transform.translation, translation);
-
-    // if (translation.length() > 1)
+    // static bool first = true;
+    // if (first)
     // {
-    //     transformStamped.transform.translation.x = 0;
-    //     transformStamped.transform.translation.y = 0;
-    //     transformStamped.transform.translation.z = 0;
+    //     first = false;
     // }
+    if (dt < 0)
+        ROS_ERROR_STREAM("accel_msg.header.stamp: " << accel_msg.header.stamp << ", transformStamped.stamp_: " << transformStamped.stamp_);
+    transformStamped.stamp_ = accel_msg.header.stamp;
 
-    double roll, pitch, yaw;
-    tf2::Matrix3x3(transformStamped.getRotation()).getRPY(roll, pitch, yaw);
-    tf2::Quaternion new_rotation;
-    new_rotation.setRPY(
-        roll + accel_msg.accel.angular.x * dt, 
-        pitch + accel_msg.accel.angular.y * dt,
-        yaw + accel_msg.accel.angular.z * dt);
-    transformStamped.setRotation(new_rotation);
+    transformStamped.frame_id_ = "map";
 
-    ROS_INFO_STREAM("sent transform");
+    UpdateTransform(accel_msg.accel, dt);
+
     auto transform_msg = tf2::toMsg(transformStamped);
-    transform_msg.child_frame_id = "mpu2";
+    transform_msg.child_frame_id = "mpu";
     br.sendTransform(transform_msg);
 }
 
@@ -56,7 +76,10 @@ int main(int argc, char **argv)
 
     ros::NodeHandle node;
 
-    transformStamped.stamp_ = ros::Time::now();
+    tf2::Quaternion rotation = transformStamped.getRotation();
+    rotation.setRPY(0, 0, 0.75 * M_PI);
+    transformStamped.setRotation(rotation);
+ 
     ros::Subscriber sub = node.subscribe("/mpu/acceleration", 1000, accelCallback);
     ros::spin();
     return 0;
