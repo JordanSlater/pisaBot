12d23be8e10adbf280177313a6f14f754506df95
Jordan Slater <slatjord@gmail.com>
2021-09-11 17:30:53 +0000

    Added notes and direction control

--- src/gpio/src/gpio_node.py
+++ src/gpio/src/gpio_node.py
@@ -17,17 +17,19 @@ MOTOR_MAX_FREQUENCY = 1500
 class MotorDriver:
     def __init__(self, speed_pin:Pin, direction_pin:Pin):
         GPIO.setup(speed_pin, GPIO.OUT)
-        GPIO.setup(direction_pin, GPIO.OUT)
         self.__speed__ = 0
         self.__pwm__ = GPIO.PWM(speed_pin, MOTOR_MAX_FREQUENCY)
+        GPIO.setup(direction_pin, GPIO.OUT)
+        self.__direction_pin__ = direction_pin
 
-    def change_speed(self, speed_msg):
+    def set_speed(self, speed_msg):
         speed = speed_msg.data
         print("setting speed to: ", speed)
-        self.__change_speed_magnitude__(abs(speed))
+        self.__set_magnitude__(abs(speed))
+        self.__set_direction__(speed > 0)
         self.__speed__ = speed
 
-    def __change_speed_magnitude__(self, speed_magnitude):
+    def __set_magnitude__(self, speed_magnitude):
         speed_was_zero = (self.__speed__ == 0)
 
         if (speed_magnitude == 0):
@@ -38,6 +40,12 @@ class MotorDriver:
         if (speed_was_zero and speed_magnitude != 0):
             self.__pwm__.start(50)
 
+    def __set_direction__(self, forward:bool):
+        if (forward):
+            gpio.output(self.__direction_pin__, gpio.HIGH)
+        else:
+            gpio.output(self.__direction_pin__, gpio.LOW)
+
     def __delete__(self):
         self.__pwm__.stop()
 
@@ -45,9 +53,9 @@ def main():
     rospy.init_node('gpio')
     GPIO.setmode(GPIO.BCM)
 
-    right_motor = MotorDriver(Pin(4))
+    right_motor = MotorDriver(Pin(4), Pin(14))
 
-    rospy.Subscriber('set_speed_right_motor', Float32, right_motor.change_speed)
+    rospy.Subscriber('set_speed_right_motor', Float32, right_motor.set_speed)
     rospy.loginfo("Motor service up.")
 
     try:
