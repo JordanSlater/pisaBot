07a16e052ded4da7e0e7c6a50cc41414e6a95904
Jordan Slater <slatjord@gmail.com>
2022-07-26 01:24:00 +0000

    WIP added pid controller

--- src/balance/src/balance_node.cpp
+++ src/balance/src/balance_node.cpp
@@ -3,13 +3,14 @@
 #include <tf2/LinearMath/Transform.h>
 #include <geometry_msgs/TransformStamped.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
-#include <visualization_msgs/Marker.h>
+#include <std_msgs/Bool.h>
+#include <std_msgs/Float64.h>
 
 inline tf2::Vector3 projectOntoPlane(tf2::Vector3 vector, tf2::Vector3 planeNormal) {
     return vector - (planeNormal.dot(vector)) * planeNormal / planeNormal.length2();
 }
 
-static float getAngleInDegrees(const geometry_msgs::TransformStamped transform) {
+static double getAngleInDegrees(const geometry_msgs::TransformStamped transform) {
     tf2::Stamped<tf2::Transform> stampedTransform;
     tf2::fromMsg(transform, stampedTransform);
 
@@ -21,10 +22,13 @@ static float getAngleInDegrees(const geometry_msgs::TransformStamped transform) 
     tf2::Vector3 globalUp{0, 0, 1};
     tf2::Vector3 targetVector = projectOntoPlane(globalUp, wheelAxis).normalize();
 
-    float angle = acos(targetVector.dot(robotUp));
+    double angle = acos(targetVector.dot(robotUp));
     int sign = (targetVector.cross(robotUp)).dot(wheelAxis) > 0 ? 1 : -1;
 
     return sign * angle * 180.0 / M_PI;
+}
+
+void balance(double angle) {
 }
 
 int main(int argc, char** argv){
@@ -35,13 +39,37 @@ int main(int argc, char** argv){
     tf2_ros::Buffer tfBuffer;
     tf2_ros::TransformListener tfListener(tfBuffer);
 
+    double targetAngleInDegrees;
+    node.param("target_angle_degrees", targetAngleInDegrees, 0.0);
+    ROS_INFO_STREAM("Loaded target_angle_degrees = " << targetAngleInDegrees);
+    
+    bool balancing = false;
+
+    ros::Publisher balancingPublisher = node.advertise<std_msgs::Bool>("balancing", 100);
+    ros::Publisher errorAnglePublisher = node.advertise<std_msgs::Float64>("balancing", 100);
+
     ros::Rate rate(10); // Hz
     while (node.ok()){
         geometry_msgs::TransformStamped transformStamped;
         try{
             transformStamped = tfBuffer.lookupTransform("map", "body", ros::Time(0));
-            float angle = getAngleInDegrees(transformStamped);
+            double angle = getAngleInDegrees(transformStamped);
             ROS_INFO_STREAM("Angle: " << angle);
+
+            if (fabs(angle + targetAngleInDegrees) < 10)
+                balancing = true;
+            else if (fabs(angle + targetAngleInDegrees) > 45)
+                balancing = false;
+
+            std_msgs::Bool balancingMsg;
+            balancingMsg.data = balancing;
+            balancingPublisher.publish((balancingMsg));
+            
+            if (balancing) {
+                std_msgs::Float64 angleMsg;
+                angleMsg.data = angle;
+                errorAnglePublisher.publish(std_msgs::Float64(angle));
+            }
         }
         catch (tf2::TransformException &ex) {
             ROS_WARN("%s", ex.what());
